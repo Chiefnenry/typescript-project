cb514b305952e288ec4c039df837a9348d9f6cb0
chiefnenry <oluwaizminelinks@gmail.com>
2024-03-06 14:38:52 +0100

    hosting it

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 let num = 25;
 const pageTitle = document.querySelector(".section-title h1");
 const container = document.querySelector(".container");
-const btnContainer = document.querySelector(".btn-container");
+const btnContainer = document.querySelector(".page-btn-container");
 const prevButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
 let fetchedData = [];
@@ -27,17 +27,21 @@ const startPagination = (data) => {
        </article>`;
     });
     container ? (container.innerHTML = displayData) : null;
-    const allPage = data.length / pageSize;
     let buttons = "";
     for (let i = 1; i <= 10; i++) {
         buttons += `<button class="page-btn">${i}</button>`;
     }
-    btnContainer
-        ? (btnContainer.innerHTML =
-            '<button class="prev-btn">prev</button>' +
-                buttons +
-                '<button class="next-btn">next</button>')
-        : null;
+    btnContainer ? (btnContainer.innerHTML = buttons) : null;
+    const pageButton = document.querySelectorAll(".page-btn");
+    pageButton.forEach((buttons) => {
+        if (Number(buttons.innerText) === currentPage) {
+            buttons.classList.add(".active-btn");
+        }
+        buttons.addEventListener("click", () => {
+            currentPage = Number(buttons.innerText);
+            startPagination(data);
+        });
+    });
 };
 const addPage = () => {
     currentPage = currentPage + 1;
@@ -49,5 +53,8 @@ const minusPage = () => {
 };
 nextButton === null || nextButton === void 0 ? void 0 : nextButton.addEventListener("click", () => {
     addPage();
+});
+prevButton === null || prevButton === void 0 ? void 0 : prevButton.addEventListener("click", () => {
+    minusPage();
 });
 const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ let num: number = 25;
 // select html element
 const pageTitle = document.querySelector(".section-title h1");
 const container = document.querySelector(".container");
-const btnContainer = document.querySelector(".btn-container");
+const btnContainer = document.querySelector(".page-btn-container");
 const prevButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
-// const btnContainer = document.querySelector(".btn-container");
 
 // fetch data
 let fetchedData: any[] = [];
@@ -27,8 +26,6 @@ const startPagination = (data: any[]) => {
 
   const newData = data.slice(startNumber, endNumber);
 
-  // console.log(newData);
-
   let displayData: string = "";
   newData.forEach((nd) => {
     displayData += `<article class="card">
@@ -40,18 +37,33 @@ const startPagination = (data: any[]) => {
 
   container ? (container.innerHTML = displayData) : null;
 
-  const allPage = data.length / pageSize;
+  // const allPage = data.length / pageSize;
   let buttons: string = "";
   for (let i = 1; i <= 10; i++) {
     buttons += `<button class="page-btn">${i}</button>`;
   }
 
-  btnContainer
-    ? (btnContainer.innerHTML =
-        '<button class="prev-btn">prev</button>' +
-        buttons +
-        '<button class="next-btn">next</button>')
-    : null;
+  btnContainer ? (btnContainer.innerHTML = buttons) : null;
+
+  const pageButton = document.querySelectorAll(".page-btn");
+  pageButton.forEach((buttons: any) => {
+    if (Number(buttons.innerText) === currentPage) {
+      buttons.classList.add(".active-btn");
+    }
+
+    buttons.addEventListener("click", () => {
+      currentPage = Number(buttons.innerText);
+      startPagination(data);
+    });
+  });
+  // console.log(pageBtns);
+
+  // btnContainer
+  //   ? (btnContainer.innerHTML =
+  //       '<button class="prev-btn">prev</button>' +
+  //       buttons +
+  //       '<button class="next-btn">next</button>')
+  //   : null;
 
   // prevButton?.addEventListener("click", () => {
   //   if (currentPage > 1) {
@@ -78,11 +90,29 @@ const minusPage = () => {
   fetchData();
 };
 
-// console.log(prevButton);
+// const numPage = () => {
+//   currentPage = currentPage + 1;
+//   fetchData();
+// };
 
 nextButton?.addEventListener("click", () => {
   addPage();
 });
+
+prevButton?.addEventListener("click", () => {
+  minusPage();
+});
+
+// btnContainer?.addEventListener("click", () => {
+//   numPage();
+// });
+
+// pageContainer?.addEventListener("click", () => {
+//   alert("worked");
+//   numPage();
+// });
+
+// console.log(pageContainer);
 
 // filter data
 const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
